8332cfc0b74f867e7d62f1a97e068418a1a4d756
davidmihai98 <davidmihai98@yahoo.com>
2019-04-09 11:56:32 +0300

    Transfering protocol with ditance sensor.

--- Code/indicator/indicator.ino
+++ Code/indicator/indicator.ino
@@ -4,8 +4,8 @@
 //led
 #define ledPin 12
 //bluetooth
-#define rxPin 8
-#define txPin 7
+#define rxPin 1
+#define txPin 0
 //distance
 #define trigPin 2
 #define echoPin 3
@@ -13,8 +13,9 @@
 #define dtPin A2 
 #define sckPin A3
 
-SoftwareSerial EEBlue(rxPin, txPin);
-Hx711 scale(dtPin, sckPin);
+  SoftwareSerial EEBlue(rxPin, txPin);
+  Hx711 scale(dtPin, sckPin);
+  char incomingByte;
 
 long calculateDistance(){
   long duration, cm;
@@ -50,12 +51,33 @@ void loop() {
   int weight = 0;
 
   distance = calculateDistance();
-  weight = scale.getGram();
+  String afisaj=String(distance)+"~";
+  //weight = scale.getGram();
 
-  EEBlue.print("d:");
-  EEBlue.println(String(distance));
-  Serial.print("distance: ");
-  Serial.println(distance);
+ if(Serial.available() >0){
+  incomingByte = Serial.read();
+  if(incomingByte == '1'){
+     digitalWrite(ledPin, HIGH);
+     /*
+     //incerc sa las sa se afiseze mai multe distante
+     int ok=10;
+     String sfarsit="~";
+     while(ok>0){
+      distance = calculateDistance();
+      String afisaj = String(distance);
+      Serial.print(afisaj);
+      Serial.print("\n");
+      ok--;
+      //delay(1000);
+     }
+     */
+     Serial.print(afisaj);
+  }
+  if(incomingByte == '0'){
+     digitalWrite(ledPin, LOW);
+  }
+ }
+ /*
   EEBlue.print("w:");
   EEBlue.println(String(weight));
   Serial.print("weight: ");
@@ -65,9 +87,6 @@ void loop() {
     digitalWrite(ledPin, HIGH);
   else
     digitalWrite(ledPin, LOW);
-
-
+  */
   delay(1000);
-  
-
 }
